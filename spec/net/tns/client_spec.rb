e3afb32aee9d1331070bf8bc55586a265b642ce7
Garrett Van Dyk <GVanDyk@Trustwave.com>
2019-05-07 17:35:46 -0400

    Added specs

---
+++ spec/net/tns/client_spec.rb
@@ -0,0 +1,22 @@
+require 'net/tns/client'
+
+module Net
+  module TNS
+    describe Client do
+      let(:vsnnum) { '301989888' }
+
+      describe "::parse_vsnnum" do
+        context "when provided a valid VSNNUM string" do
+          it "should return the correct version" do
+            expect(Client.parse_vsnnum(vsnnum)).to eql?('18.0.0.0.0')
+          end
+        end
+        context "when provided a non-string" do
+          it "should raise an error" do
+            expect { Client.parse_vsnnum(12345678) }.to raise_error(ArgumentError)
+          end
+        end
+      end
+    end
+  end
+end
